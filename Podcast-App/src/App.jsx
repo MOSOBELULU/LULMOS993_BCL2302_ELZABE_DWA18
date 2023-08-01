1c900b44e6ce1319a588209adc6bc0df2d03b71f
MOSOBELULU <mosobelulu@gmail.com>
2023-08-01 15:13:21 +0200

    updated

---
+++ Podcast-App/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import PosterList from './components/PosterList'
+import { NavBar } from './components/NavBar'
+
+ export default function App() {
+  return(
+    <div>
+      <NavBar />
+      <PosterList />
+    </div>
+  )
+ }
